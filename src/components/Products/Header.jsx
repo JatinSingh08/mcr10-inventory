eff3839dde16656d429ef64df775baa07c03361b
JatinSingh08 <singhjatin0812@gmail.com>
2023-08-12 22:25:59 +0530

    fix: low items sort

--- src/components/Products/Header.jsx
+++ src/components/Products/Header.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { useData } from '../../context/data/data-context';
-import { ActionType } from '../../reducers/constants';
+import React from "react";
+import { useData } from "../../context/data/data-context";
+import { ActionType } from "../../reducers/constants";
 
 const Header = () => {
   const { state, dispatch } = useData();
 
   const filtersHandler = (e) => {
     const filterType = e.target.name;
-    const filterValue = e.target.value;
-
-    dispatch({ type: ActionType.CHANGE_FILTER, payload: {filterType, filterValue}})
-  }
+    let filterValue = e.target.value;
+    if (filterType === "lowStockItems") filterValue = e.target.checked;
+    dispatch({
+      type: ActionType.CHANGE_FILTER,
+      payload: { filterType, filterValue },
+    });
+  };
 
   return (
-    <div className='flex justify-between items-center border px-6 py-4'>
-      <div className='text-2xl font-bold '>
+    <div className="flex justify-between items-center border px-6 py-4">
+      <div className="text-2xl font-bold ">
         <h1>Products</h1>
       </div>
       <div>
-        <select id="" className='bg-gray-200 p-2 rounded-lg'
-        name='category'
-        value={state.filters.category}
-        onChange={filtersHandler}
+        <select
+          id=""
+          className="bg-gray-200 p-2 rounded-lg"
+          name="category"
+          value={state.filters.category}
+          onChange={filtersHandler}
         >
           <option value="All">All Departments</option>
           <option value="Kitchen">Kitchen</option>
@@ -30,17 +35,25 @@ const Header = () => {
         </select>
       </div>
       <div>
-        <label htmlFor="low__stock" className='gap-2 flex items-center justify-center'>
-          <input type="checkbox" id='low__stock' name='lowStockItems' value={state.filters.lowStockItems}
-          onChange={filtersHandler}
+        <label
+          htmlFor="low__stock"
+          className="gap-2 flex items-center justify-center"
+        >
+          <input
+            type="checkbox"
+            id="low__stock"
+            name="lowStockItems"
+            value={state.filters.lowStockItems}
+            onChange={filtersHandler}
           />
           Low Stock Items
         </label>
       </div>
-      <div >
-        <select className='bg-gray-200 p-2 rounded-lg'
-        name='sort'
-        onChange={filtersHandler}
+      <div>
+        <select
+          className="bg-gray-200 p-2 rounded-lg"
+          name="sort"
+          onChange={filtersHandler}
         >
           <option value="name">Name</option>
           <option value="price">Price</option>
@@ -48,10 +61,12 @@ const Header = () => {
         </select>
       </div>
       <div>
-        <button className='bg-blue-600 text-white px-6 py-2 rounded-lg'>New</button>
+        <button className="bg-blue-600 text-white px-6 py-2 rounded-lg">
+          New
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
