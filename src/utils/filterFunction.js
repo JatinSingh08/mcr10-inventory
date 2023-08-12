b5b4c392ba12a128dff28ae1159587b2ae25aa4a
JatinSingh08 <singhjatin0812@gmail.com>
2023-08-12 22:21:58 +0530

    fix: sort by name

--- src/utils/filterFunction.js
+++ src/utils/filterFunction.js
@@ -1,10 +1,26 @@
 const filtersHandler = (state) => {
-  const { productsData ,filters: { category, lowStockItems, sort} } = state;
+  const {
+    productsData,
+    filters: { category, lowStockItems, sort },
+  } = state;
   let newData = productsData;
-  newData = category === "All" ? newData : newData.filter(data => data.department === category);
-  newData = lowStockItems ? newData.filter(data => data.stock <= 10) : newData;
-  newData = sort ? newData.sort((a, b) => sort === "name" ? a.name - b.name : sort === "price" ? a.price - b.price : a.stock - b.stock) : newData;
+  newData =
+    category === "All"
+      ? newData
+      : newData.filter((data) => data.department === category);
+  newData = lowStockItems
+    ? newData.filter((data) => data.stock <= 10)
+    : newData;
+  newData = sort
+    ? newData.sort((a, b) =>
+        sort === "name"
+          ? a.name.localeCompare(b.name)
+          : sort === "price"
+          ? a.price - b.price
+          : a.stock - b.stock
+      )
+    : newData;
   return newData;
-}
+};
 
 export { filtersHandler };
