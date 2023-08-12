129446b606b89421414d9e4c16312ed24b4b2ee3
JatinSingh08 <singhjatin0812@gmail.com>
2023-08-12 22:12:15 +0530

    add: sort

--- src/utils/filterFunction.js
+++ src/utils/filterFunction.js
@@ -3,7 +3,7 @@ const filtersHandler = (state) => {
   let newData = productsData;
   newData = category === "All" ? newData : newData.filter(data => data.department === category);
   newData = lowStockItems ? newData.filter(data => data.stock <= 10) : newData;
-  newData = sort ? newData.sort((a, b) => a["sort"] - b["sort"]) : newData;
+  newData = sort ? newData.sort((a, b) => sort === "name" ? a.name - b.name : sort === "price" ? a.price - b.price : a.stock - b.stock) : newData;
   return newData;
 }
 
